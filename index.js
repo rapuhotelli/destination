625229e473f2bbfb8eb9ab76818b08b96960454a
Jukka Lähetkangas <jukka.lahetkangas@fraktio.fi>
2020-04-25 14:22:40 +0300

    cached centered

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ const altAngleRad = Math.PI * 2 / altTriangleCount
 const rotationAngle = Math.PI * 2 / 360
 
 let screenRadius = 1;
+let screenDiagonal = 1;
 let longestEdge = 0;
 let fadeCircleGradient;
+
+let oscLayers = []
+
+let center = {
+  x: 0,
+  y: 0,
+  zeroOffsetX: 0,
+  zeroOffsetY: 0
+}
+
 
 const triangleRayColors = [
   'rgb(240, 120, 20)',//'#FF6B49',
@@ -19,75 +30,112 @@ const triangleRayColors = [
   '#E8D20C' // 'rgba(255,250,128,1)'
 ]
 
-const triangle = (i, tick, alt) => {
-  if (alt && i%2 === 0) return
+const triangle = (context, i, tick, alt) => {
+  if (alt && i % 2 === 0) return
   const angle = alt ? altAngleRad : angleRad
   const color = alt ? 'rgb(255, 255, 0)' : triangleRayColors[i % 3]
-  ctx.save()
+  context.save()
   
   if (alt) {
-    // ctx.translate(5, 5)
-    ctx.rotate(rotationAngle * tick/4 / 40)
+    context.rotate(rotationAngle * tick/4 / 40)
   } else {
-    ctx.rotate(rotationAngle * tick / 40)
+    context.rotate(rotationAngle * tick / 40)
   }
-  ctx.beginPath();
-  ctx.lineWidth = 1
-  ctx.strokeStyle = color
-  ctx.fillStyle = color
-  // ctx.translate(tick, 0)
-  ctx.moveTo(0, 0)
-  ctx.lineTo(Math.floor(Math.cos(angle*i)*screenRadius), Math.floor(Math.sin(angle*i)*(screenRadius)))
-  ctx.lineTo(Math.floor(Math.cos(angle*(i+1))*screenRadius), Math.floor(Math.sin(angle*(i+1))*(screenRadius)))
-  ctx.fill()
-  ctx.stroke()
-  ctx.restore()
+  context.beginPath();
+  context.lineWidth = 1
+  context.strokeStyle = color
+  context.fillStyle = color
+  // context.translate(tick, 0)
+  context.moveTo(0, 0)
+  context.lineTo(Math.floor(Math.cos(angle*i)*screenRadius), Math.floor(Math.sin(angle*i)*(screenRadius)))
+  context.lineTo(Math.floor(Math.cos(angle*(i+1))*screenRadius), Math.floor(Math.sin(angle*(i+1))*(screenRadius)))
+  context.fill()
+  context.stroke()
+  context.restore()
 }
 
-const fadeOutCircle = () => {
-  ctx.save()
-  ctx.fillStyle = fadeCircleGradient
-  ctx.arc(0, 0, longestEdge, 0, Math.PI*2)
-  ctx.fill()
-  ctx.restore()
+
+function createOffscreenCanvas() {
+  console.log(Math.sqrt(Math.pow(ctx.canvas.width/2, 2) * Math.pow(ctx.canvas.height/2, 2)))
+  const canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth   //window.innerWidth / 2
+  canvas.height = window.innerHeight //window.innerWidth / 2
+  canvas.ctx = canvas.getContext('2d')
+  canvas.ctx.translate(canvas.width / 2, canvas.height / 2)
+  return canvas
 }
 
 function init() {
   canvas = document.getElementById('canvas')
   ctx = canvas.getContext('2d')
   ctx.canvas.width  = window.innerWidth / 2
-  ctx.canvas.height = window.innerHeight / 2;
-  // screenRadius = ctx.canvas.width / 2;
+  ctx.canvas.height = window.innerHeight / 2
+
   longestEdge = Math.max(ctx.canvas.width, ctx.canvas.height)
   screenRadius = Math.floor(Math.sqrt(Math.pow(ctx.canvas.width/2, 2) * Math.pow(ctx.canvas.height/2, 2)))
   
-  ctx.translate(Math.floor(ctx.canvas.width * 0.7), Math.floor(ctx.canvas.height * 0.7))
+  screenDiagonal = Math.floor(Math.sqrt(Math.pow(ctx.canvas.width, 2) * Math.pow(ctx.canvas.height, 2)))
   
-  fadeCircleGradient = ctx.createRadialGradient(0, 0, longestEdge / 4, 0.001, 0.0001, longestEdge / 2)
-  fadeCircleGradient.addColorStop(0, 'white')
-  fadeCircleGradient.addColorStop(1, 'blue')
+  console.log(screenRadius, screenDiagonal)
+  
+  center.x = Math.floor(ctx.canvas.width * 0.5) // Math.floor(ctx.canvas.width * 0.7)
+  center.y = Math.floor(ctx.canvas.height * 0.5)// Math.floor(ctx.canvas.height * 0.7)
+  ctx.translate(center.x, center.y)
+  
+  oscLayers[0] = createOffscreenCanvas()
+  for (let i = 0; i < triangleCount; i++) {
+    triangle(oscLayers[0].ctx, i, 0)
+  }
+  
+  oscLayers[1] = createOffscreenCanvas()
+  for (let i = 0; i < altTriangleCount; i++) {
+    triangle(oscLayers[1].ctx, i, 0, true)
+  }
+  
+  console.log({
+    window: {
+      width: window.innerWidth,
+      height: window.innerHeight
+    },
+    mainCanvas: {
+      width: canvas.width,
+      height: canvas.height
+    }
+  })
   
   window.requestAnimationFrame(draw)
 }
 
-function spinningBackgroundThing(tick) {
-  for (let i = 0; i < triangleCount; i++) {
-    triangle(i, tick)
-  }
-  for (let i = 0; i < altTriangleCount; i++) {
-    triangle(i, tick, true)
-  }
+function copyCachedLayer(layer) {
+  ctx.drawImage(
+    layer,
+    0,
+    0,
+    layer.width,
+    layer.height,
+    -ctx.canvas.width / 2 - layer.width / 4,
+    -ctx.canvas.height / 2 - layer.height / 4,
+    layer.width,
+    layer.height
+  );
 }
 
 function draw(tick) {
   ctx.save()
-  ctx.clearRect(-ctx.canvas.width/2, -ctx.canvas.height/2, ctx.canvas.width, ctx.canvas.height)
+  ctx.clearRect(-center.x, -center.y, ctx.canvas.width, ctx.canvas.height)
   
-  spinningBackgroundThing(tick) // tick
+  ctx.save()
+  // ctx.translate(100,100)
+  // ctx.rotate(rotationAngle * tick / 40)
+  copyCachedLayer(oscLayers[0])
+  ctx.restore()
   
-   // */
+  ctx.save()
+  // ctx.translate(100,100)
+  // ctx.rotate(rotationAngle * tick/4 / 40)
+  copyCachedLayer(oscLayers[1])
+  ctx.restore()
   
-  // fadeOutCircle()
   ctx.restore()
   window.requestAnimationFrame(draw)
 }
