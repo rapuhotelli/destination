f0a7a9ff581732ef5bf11c984fc2574773d0e5dc
Jukka Lähetkangas <jukka.lahetkangas@fraktio.fi>
2020-04-25 19:36:46 +0300

    car

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const altAngleRad = Math.PI * 2 / altTriangleCount
 const rotationAngle = Math.PI * 2 / 360
 
 let screenRadius = 1;
-let screenDiagonal = 1;
-let longestEdge = 0;
 let fadeCircleGradient;
-
 let oscLayers = []
+let carImage;
 
 let center = {
   x: 0,
@@ -22,7 +20,6 @@ let center = {
   zeroOffsetX: 0,
   zeroOffsetY: 0
 }
-
 
 const triangleRayColors = [
   'rgb(240, 120, 20)',//'#FF6B49',
@@ -45,7 +42,6 @@ const triangle = (context, i, tick, alt) => {
   context.lineWidth = 1
   context.strokeStyle = color
   context.fillStyle = color
-  // context.translate(tick, 0)
   context.moveTo(0, 0)
   context.lineTo(Math.floor(Math.cos(angle*i)*screenRadius), Math.floor(Math.sin(angle*i)*(screenRadius)))
   context.lineTo(Math.floor(Math.cos(angle*(i+1))*screenRadius), Math.floor(Math.sin(angle*(i+1))*(screenRadius)))
@@ -53,7 +49,6 @@ const triangle = (context, i, tick, alt) => {
   context.stroke()
   context.restore()
 }
-
 
 function createOffscreenCanvas() {
   console.log(Math.sqrt(Math.pow(ctx.canvas.width/2, 2) * Math.pow(ctx.canvas.height/2, 2)))
@@ -71,41 +66,42 @@ function init() {
   ctx.canvas.width  = window.innerWidth / 2
   ctx.canvas.height = window.innerHeight / 2
 
-  longestEdge = Math.max(ctx.canvas.width, ctx.canvas.height)
   screenRadius = Math.floor(Math.sqrt(Math.pow(ctx.canvas.width/2, 2) * Math.pow(ctx.canvas.height/2, 2)))
   
-  screenDiagonal = Math.floor(Math.sqrt(Math.pow(ctx.canvas.width, 2) * Math.pow(ctx.canvas.height, 2)))
-  
-  console.log(screenRadius, screenDiagonal)
-  
-  center.x = Math.floor(ctx.canvas.width * 0.5) // Math.floor(ctx.canvas.width * 0.7)
-  center.y = Math.floor(ctx.canvas.height * 0.5)// Math.floor(ctx.canvas.height * 0.7)
+  center.x = Math.floor(ctx.canvas.width * 0.5)
+  center.y = Math.floor(ctx.canvas.height * 0.5)
   center.zeroOffsetX = ctx.canvas.width * 0.7 - center.x
   center.zeroOffsetY = ctx.canvas.height * 0.7 - center.y
   ctx.translate(center.x, center.y)
   
+  fadeCircleGradient = ctx.createRadialGradient(
+    center.zeroOffsetX,
+    center.zeroOffsetY,
+    ctx.canvas.width * 0.4,
+    center.zeroOffsetX,
+    center.zeroOffsetY,
+    ctx.canvas.width * 0.6
+  );
+
+  fadeCircleGradient.addColorStop(0, 'rgba(255, 255, 0, 0)');
+  fadeCircleGradient.addColorStop(0.8, 'rgba(255, 255, 0.5)');
+  fadeCircleGradient.addColorStop(1, 'rgba(255, 255, 0, 1)');
+
   oscLayers[0] = createOffscreenCanvas()
   for (let i = 0; i < triangleCount; i++) {
     triangle(oscLayers[0].ctx, i, 0)
   }
-  
+
   oscLayers[1] = createOffscreenCanvas()
   for (let i = 0; i < altTriangleCount; i++) {
     triangle(oscLayers[1].ctx, i, 0, true)
   }
-  
-  console.log({
-    window: {
-      width: window.innerWidth,
-      height: window.innerHeight
-    },
-    mainCanvas: {
-      width: canvas.width,
-      height: canvas.height
-    }
+
+  carImage = new Image()
+  carImage.addEventListener('load', function () {
+    window.requestAnimationFrame(draw)
   })
-  
-  window.requestAnimationFrame(draw)
+  carImage.src = 'assets/carsprite-lg.png'
 }
 
 function copyCachedLayer(layer) {
@@ -120,6 +116,20 @@ function copyCachedLayer(layer) {
     layer.width,
     layer.height
   );
+}
+
+function carAnimation(frame) {
+  ctx.drawImage(
+    carImage,
+    (frame % 2 === 0) ? 0 : 188,
+    0,
+    188,
+    68,
+    center.zeroOffsetX - 94,
+    center.zeroOffsetY - 34,
+    188,
+    68
+  )
 }
 
 function draw(tick) {
@@ -137,6 +147,13 @@ function draw(tick) {
   ctx.rotate(rotationAngle * tick/4 / 40)
   copyCachedLayer(oscLayers[1])
   ctx.restore()
+  
+  ctx.save()
+  ctx.fillStyle = fadeCircleGradient;
+  ctx.fillRect(-center.x, -center.y, ctx.canvas.width, ctx.canvas.height);
+  ctx.restore()
+  
+  carAnimation(Math.floor(tick / 300) % 2)
   
   ctx.restore()
   window.requestAnimationFrame(draw)
